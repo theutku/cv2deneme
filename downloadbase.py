b51d79c489904f457783e71c008970be48ac5507
utku-aibrite <utku@aibrite.com>
2017-10-04 01:37:31 +0300

    added ugly image remover for cascade

--- downloadbase.py
+++ downloadbase.py
@@ -4,39 +4,72 @@ import numpy as np
 import os
 
 
-class CascadeImageDownloader():
+class DownloadPath():
 
-    def __init__(self):
+    def __init__(self, download_dir):
+        self.download_dir = download_dir
+        self.dirs = {
+            'main': self.download_dir,
+            'positive': os.path.join(self.download_dir, 'pos'),
+            'negative': os.path.join(self.download_dir, 'neg'),
+            'uglies': os.path.join(self.download_dir, 'uglies')
+        }
+
         self._check_directories()
 
     def _check_directories(self):
-        if not os.path.exists('downloads'):
-            os.makedirs('downloads')
-        if not os.path.exists('downloads/neg'):
-            os.makedirs('downloads/neg')
-        if not os.path.exists('downloads/pos'):
-            os.makedirs('downloads/pos')
 
-    def _resize_image(self, image):
+        for key, value in self.dirs.items():
+            if not os.path.exists(self.dirs[key]):
+                os.makedirs(self.dirs[key])
+
+
+class CascadeImageProcessor(DownloadPath):
+
+    def __init__(self, download_dir='downloads'):
+        super().__init__(download_dir)
+
+    def resize_image(self, image):
         resized = cv2.resize(image, (100, 100))
         return resized
 
-    def _grayscale_and_save(self, file_name):
+    def grayscale_and_save(self, file_name):
         gray = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
-        resized = self._resize_image(gray)
+        resized = self.resize_image(gray)
         cv2.imwrite(file_name, resized)
-        print('Image Saved')
+        print('Resized Grayscale Image Saved')
 
-    def _download_and_process(self, urls, pos=False, count=None):
+    def remove_uglies(self):
+        for sign_path in os.listdir(self.dirs['main']):
+            for img in os.listdir(sign_path):
+                for ugly in os.listdir(self.dirs['uglies']):
+                    current_img_path = str(sign_path) + '/' + img
+                    ugly = cv2.imread(self.dirs['uglies'] + ugly)
+                    current_img = cv2.imread(current_img_path)
+
+                    if ugly.shape == img.shape and not (np.bitwise_xor(ugly, current_img).any()):
+                        print('Ugly image found: {}'.format(current_img_path))
+                        print('Image removed')
+                        os.remove(current_img_path)
+
+
+class CascadeImageDownloader(CascadeImageProcessor):
+
+    def download_and_process(self, urls, pos=False, count=None):
         pic_count = count + 1
-        base_url = 'downloads/neg/' if pos is False else 'downloads/pos/'
+        base_url = self.dirs['negative'] if pos is False else self.dirs['positive']
 
         for image_url in urls.split('\n'):
+            # if os.path.exists(base_url + str(pic_count) + '.jpg'):
+            #     print(pic_count)
+            #     pic_count += 1
+            #     continue
+
             try:
-                print(image_url)
+                print('Downloading Image No {}: {}'.format(pic_count, image_url))
                 urllib.request.urlretrieve(
                     image_url, base_url + str(pic_count) + '.jpg')
-                self._grayscale_and_save(
+                self.grayscale_and_save(
                     base_url + str(pic_count) + '.jpg')
                 pic_count += 1
 
@@ -51,12 +84,12 @@ class CascadeImageDownloader():
 
         for neg_url in neg_urls:
             neg_image_urls = urllib.request.urlopen(neg_url).read().decode()
-            last_neg = self._download_and_process(
+            last_neg = self.download_and_process(
                 neg_image_urls, count=last_neg)
 
         for pos_url in pos_urls:
             pos_image_urls = urllib.request.urlopen(pos_url).read().decode()
-            last_pos = self._download_and_process(
+            last_pos = self.download_and_process(
                 pos_image_urls, pos=True, count=last_pos)
 
         # pic_count = 1
@@ -71,6 +104,8 @@ class CascadeImageDownloader():
 
         #     except Exception as err:
         #         print(str(err))
+
+    # def remove_uglies(self):
 
 
 cas = CascadeImageDownloader()
